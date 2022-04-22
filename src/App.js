d7c7c8d247fdfeb87b036b1c010978fe7bd733f5
mfflynnnn <yougotanemailfrommolly@gmail.com>
2022-04-21 23:15:46 -0700

    Create site
    Oops I forgot to commit this from the get-go and along the way. Whomp whomp!

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Logo from "./purple_cup_lounge_logo.jpg";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="container">
+          <div className="row top">
+            <div className="col-xl-5 logo">
+              <img src={Logo} alt="Logo"></img>
+            </div>
+            <div className="col-xl-7 text">
+              <h1>Purple Cup Lounge</h1>
+              <h2>Seattle's First Dry Bar</h2>
+              <h3>Coming Soon!</h3>
+            </div>
+          </div>
+          <div className="row bottom">
+            <div className="col-2"></div>
+            <div className="col tagline">
+              <h4>
+                Purple Cup Lounge is a place where you can enjoy all that bars
+                have to offer - all 100% alcohol-free!
+              </h4>
+            </div>
+            <div className="col-2"></div>
+          </div>
+        </div>
       </header>
     </div>
   );
 }
-
-export default App;
